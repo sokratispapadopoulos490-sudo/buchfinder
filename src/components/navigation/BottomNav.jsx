fdf7449317f51c76a7766e4e3bcf87013ea33603
Sokratis Papadopoulos <sokratis.papadopoulos490@googlemail.com>
2026-02-12 09:50:57 +0000

    File changes

--- src/components/navigation/BottomNav.jsx
+++ src/components/navigation/BottomNav.jsx
@@ -42,7 +42,7 @@ export default function BottomNav() {
   return (
     <div style={{
       position: 'fixed',
-      bottom: '40px',
+      bottom: '80px',
       left: '8px',
       right: '8px',
       backgroundColor: 'white',
